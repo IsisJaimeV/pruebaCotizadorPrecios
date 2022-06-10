9ef8567324a1c0d0fb3661e018fad23d80af2113
ijaime <ijaime@mcllent.com>
2022-06-10 00:46:16 -0500

    Grafica resultados

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
+ 
   constructor() { }
 
   ngOnInit(): void {
+    this.gaugeChart();
   }
 
+  gaugeChart(){
+    
+  }
 }
