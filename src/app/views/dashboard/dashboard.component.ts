1a93ec384f3d21a5c932851a3eb4aa02cd9e14ab
ijaime <ijaime@mcllent.com>
2022-06-14 20:31:48 -0500

    Revert "Desbloqueo de campo precio propuesto"
    This reverts commit 26217d7ef0a17e90a9f7fd403a6f2de3732e891f.

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -90,15 +90,12 @@ export class DashboardComponent implements OnInit {
   //DIFERENCIA PARTE INFERIOR
   costoVariable: string = '-';
 
-  //NG MODEL
-  selectedCodigoSpan: string = '-';
-
   //FORM
   filterForm = new FormGroup({
     linea: new FormControl('', Validators.required),
     codigo: new FormControl('', Validators.required),
     zona: new FormControl('', Validators.required),
-    propuesto: new FormControl('', Validators.nullValidator),
+    propuesto: new FormControl('', Validators.required),
     volumen: new FormControl('', Validators.required),
     tipoOperacion: new FormControl(false, Validators.nullValidator),
   })
@@ -124,195 +121,18 @@ export class DashboardComponent implements OnInit {
     });
   }
 
-  selectedCodigo(event: any) {
-    let value = event.target.value;
-    console.log(value);
-  }
-
   selectZona() {
     this.precioPiso.getZona().subscribe(res => {
       this.zona = res;
     });
   }
+
   loader() {
     //ACTIVA LOADER
     this.spinner.show();
     setTimeout(() => {
       this.spinner.hide();
     }, 2000);
-
-  }
-
-  loaderCheckbox(form: getDatosI) {
-    //ACTIVA LOADER
-    this.spinner.show();
-    setTimeout(() => {
-      this.spinner.hide();
-    }, 2000);
-
-    this.precioPiso.getDatos(form).subscribe(res => {
-
-      // COSTO PRECIO PISO
-      this.preciopisoGral = res.resultado.info.precioPiso.toFixed(2);
-      this.costoVta = res.resultado.info.costoVta.toFixed(2);
-      this.gastoCryo = res.resultado.info.gastoCryo.toFixed(2);
-      this.gastoDist = res.resultado.info.gastoDist.toFixed(2);
-      this.depreciacion = res.resultado.info.depreciacion.toFixed(2);
-      this.utilidadOperativaSinGVyGADM = res.resultado.info.utilidadOperativaSinGVyGADM.toFixed(2);
-      this.gastoVta = res.resultado.info.gastoVta.toFixed(2);
-      this.gastoAdm = res.resultado.info.gastoAdm.toFixed(2);
-      this.utilidadOperativaNeta = res.resultado.info.utilidadOperativaNeta.toFixed(2);
-
-      // PORCENTAJE PRECIO PISO
-      this.porpreciopisoGral = res.resultado.porcentajePrecioPiso.precioPiso.toFixed(1);
-      this.porcostoVta = res.resultado.porcentajePrecioPiso.costoVta.toFixed(1);
-      this.porgastoCryo = res.resultado.porcentajePrecioPiso.gastoCryo.toFixed(1);
-      this.porgastoDist = res.resultado.porcentajePrecioPiso.gastoDist.toFixed(1);
-      this.pordepreciacion = res.resultado.porcentajePrecioPiso.depreciacion.toFixed(1);
-      this.porutilidadOperativaSinGVyGADM = res.resultado.porcentajePrecioPiso.utilidadOperativaSinGVyGADM.toFixed(1);
-      this.porgastoVta = res.resultado.porcentajePrecioPiso.gastoVta.toFixed(1);
-      this.porgastoAdm = res.resultado.porcentajePrecioPiso.gastoAdm.toFixed(1);
-      this.porutilidadOperativaNeta = res.resultado.porcentajePrecioPiso.utilidadOperativaNeta.toFixed(1);
-
-      // COSTO PRECIO PROPUESTO
-      this.pppreciopisoGral = res.resultado.infoPropuesto.precioPiso.toFixed(2);
-      this.ppcostoVta = res.resultado.infoPropuesto.costoVta.toFixed(2);
-      this.ppgastoCryo = res.resultado.infoPropuesto.gastoCryo.toFixed(2);
-      this.ppgastoDist = res.resultado.infoPropuesto.gastoDist.toFixed(2);
-      this.ppdepreciacion = res.resultado.infoPropuesto.depreciacion.toFixed(2);
-      this.pputilidadOperativaSinGVyGADM = res.resultado.infoPropuesto.utilidadOperativaSinGVyGADM.toFixed(2);
-      this.ppgastoVta = res.resultado.infoPropuesto.gastoVta.toFixed(2);
-      this.ppgastoAdm = res.resultado.infoPropuesto.gastoAdm.toFixed(2);
-      this.pputilidadOperativaNeta = res.resultado.infoPropuesto.utilidadOperativaNeta.toFixed(2);
-
-      //UTILIDAD OPERATIVA NETA
-      const utilidad = this.pputilidadOperativaNeta;
-      if (utilidad >= 0) {
-        this.utilidadNeta = 'Positiva "Creación de Valor"';
-        (document.getElementById('utilidadNetaText') as HTMLDivElement).style.color = "green";
-      } else {
-        this.utilidadNeta = 'Negativa "Destrucción de Valor"';
-        (document.getElementById('utilidadNetaText') as HTMLDivElement).style.color = "red";
-      }
-
-      //GAUGE CHART
-      this.chartPrecioPropuesto = this.pppreciopisoGral;
-      this.chartPrecioPiso = this.preciopisoGral;
-      this.chartCostoVenta = res.resultado.graficaDto.costoVenta;
-      this.chartGastoCryogenico = res.resultado.graficaDto.gastoCryDep;
-      this.chartGastosVenta = res.resultado.graficaDto.gvAdmin;
-
-      const colorAngulo = res.resultado.graficaDto.color;
-      if (colorAngulo == 0) { // 0 ->Error de calculo
-        this.angulo = -5;
-      } if (colorAngulo == 1) { // 1 ->Verde
-        this.angulo = 170;
-      } if (colorAngulo == 2) { // 2 ->Rojo
-        this.angulo = 10;
-      } if (colorAngulo == 3) { // 3 ->Amarillo
-        this.angulo = 120;
-      } if (colorAngulo == 4) { // 4 ->Naranja
-        this.angulo = 60;
-      }
-
-      this.costoVariable = res.resultado.graficaDto.analisisOportunidad;
-
-      // PORCENTAJE PRECIO PROPUESTO
-      this.ppporpreciopisoGral = res.resultado.porcentajePropuesto.precioPiso.toFixed(1);
-      this.ppporcostoVta = res.resultado.porcentajePropuesto.costoVta.toFixed(1);
-      this.ppporgastoCryo = res.resultado.porcentajePropuesto.gastoCryo.toFixed(1);
-      this.ppporgastoDist = res.resultado.porcentajePropuesto.gastoDist.toFixed(1);
-      this.pppordepreciacion = res.resultado.porcentajePropuesto.depreciacion.toFixed(1);
-      this.ppporutilidadOperativaSinGVyGADM = res.resultado.porcentajePropuesto.utilidadOperativaSinGVyGADM.toFixed(1);
-      this.ppporgastoVta = res.resultado.porcentajePropuesto.gastoVta.toFixed(1);
-      this.ppporgastoAdm = res.resultado.porcentajePropuesto.gastoAdm.toFixed(1);
-      this.ppporutilidadOperativaNeta = res.resultado.porcentajePropuesto.utilidadOperativaNeta.toFixed(1);
-
-      // TOTALES PRECIO PISO
-      this.tpfacturacionAnual = res.resultado.totales.facturacionAnual;
-      this.tputilidadOperativaNeta = res.resultado.totales.utilidadOperativaNeta;
-      this.tputilidadOperativaNetaCryoInfra = res.resultado.totales.utilidadOperativaNetaCryoInfra;
-      this.tputilidadOperativaSinGVyGADM = res.resultado.totales.utilidadOperativaSinGVyGADM;
-
-      // TOTALES PRECIO PROPUESTO
-      this.tppfacturacionAnual = res.resultado.totalesPropuesto.facturacionAnual;
-      this.tpputilidadOperativaNeta = res.resultado.totalesPropuesto.utilidadOperativaNeta;
-      this.tpputilidadOperativaNetaCryoInfra = res.resultado.totalesPropuesto.utilidadOperativaNetaCryoInfra;
-      this.tpputilidadOperativaSinGVyGADM = res.resultado.totalesPropuesto.utilidadOperativaSinGVyGADM;
-
-      //DIFERENCIA UTILIDAD PRECIO PROPUESTO VS PISO
-      this.difPrePropuestoVSPrePiso = res.resultado.graficaDto.precioPropuestoVPiso;
-
-      if (this.difPrePropuestoVSPrePiso >= 0) {
-        (document.getElementById('difPrePropuestoVSPrePiso') as HTMLDivElement).style.color = "green";
-      } else {
-        (document.getElementById('difPrePropuestoVSPrePiso') as HTMLDivElement).style.color = "red";
-      }
-
-    }, (errorServicio) => {
-      console.log(errorServicio);
-      Swal.fire(
-        'Intenta nuevamente',
-        'La consulta no fue validada',
-        'error'
-      )
-      this.preciopisoGral = 0;
-      this.costoVta = 0;
-      this.gastoCryo = 0;
-      this.gastoDist = 0;
-      this.depreciacion = 0
-      this.utilidadOperativaSinGVyGADM = 0;
-      this.gastoVta = 0;
-      this.gastoAdm = 0;
-      this.utilidadOperativaNeta = 0;
-
-      // PORCENTAJE PRECIO PISO
-      this.porpreciopisoGral = 0;
-      this.porcostoVta = 0;
-      this.porgastoCryo = 0;
-      this.porgastoDist = 0;
-      this.pordepreciacion = 0;
-      this.porutilidadOperativaSinGVyGADM = 0;
-      this.porgastoVta = 0;
-      this.porgastoAdm = 0;
-      this.porutilidadOperativaNeta = 0;
-
-      // COSTO PRECIO PROPUESTO
-      this.pppreciopisoGral = 0;
-      this.ppcostoVta = 0;
-      this.ppgastoCryo = 0;
-      this.ppgastoDist = 0;
-      this.ppdepreciacion = 0;
-      this.pputilidadOperativaSinGVyGADM = 0;
-      this.ppgastoVta = 0;
-      this.ppgastoAdm = 0;
-      this.pputilidadOperativaNeta = 0;
-
-      // PORCENTAJE PRECIO PROPUESTO
-      this.ppporpreciopisoGral = 0;
-      this.ppporcostoVta = 0;
-      this.ppporgastoCryo = 0;
-      this.ppporgastoDist = 0;
-      this.pppordepreciacion = 0;
-      this.ppporutilidadOperativaSinGVyGADM = 0;
-      this.ppporgastoVta = 0;
-      this.ppporgastoAdm = 0;
-      this.ppporutilidadOperativaNeta = 0;
-
-      // TOTALES PRECIO PISO
-      this.tpfacturacionAnual = 0;
-      this.tputilidadOperativaNeta = 0;
-      this.tputilidadOperativaNetaCryoInfra = 0;
-      this.tputilidadOperativaSinGVyGADM = 0;
-
-      // TOTALES PRECIO PROPUESTO
-      this.tppfacturacionAnual = 0;
-      this.tpputilidadOperativaNeta = 0;
-      this.tpputilidadOperativaNetaCryoInfra = 0;
-      this.tpputilidadOperativaSinGVyGADM = 0;
-    })
-
-
   }
 
   consultarDatos(form: getDatosI) {
@@ -380,9 +200,9 @@ export class DashboardComponent implements OnInit {
       } if (colorAngulo == 4) { // 4 ->Naranja
         this.angulo = 60;
       }
-
+      
       this.costoVariable = res.resultado.graficaDto.analisisOportunidad;
-
+      
       // PORCENTAJE PRECIO PROPUESTO
       this.ppporpreciopisoGral = res.resultado.porcentajePropuesto.precioPiso.toFixed(1);
       this.ppporcostoVta = res.resultado.porcentajePropuesto.costoVta.toFixed(1);
@@ -408,10 +228,10 @@ export class DashboardComponent implements OnInit {
 
       //DIFERENCIA UTILIDAD PRECIO PROPUESTO VS PISO
       this.difPrePropuestoVSPrePiso = res.resultado.graficaDto.precioPropuestoVPiso;
-
-      if (this.difPrePropuestoVSPrePiso >= 0) {
+      
+      if( this.difPrePropuestoVSPrePiso >= 0){
         (document.getElementById('difPrePropuestoVSPrePiso') as HTMLDivElement).style.color = "green";
-      } else {
+      }else{
         (document.getElementById('difPrePropuestoVSPrePiso') as HTMLDivElement).style.color = "red";
       }
 
@@ -422,60 +242,6 @@ export class DashboardComponent implements OnInit {
         'La consulta no fue validada',
         'error'
       )
-      this.preciopisoGral = 0;
-      this.costoVta = 0;
-      this.gastoCryo = 0;
-      this.gastoDist = 0;
-      this.depreciacion = 0
-      this.utilidadOperativaSinGVyGADM = 0;
-      this.gastoVta = 0;
-      this.gastoAdm = 0;
-      this.utilidadOperativaNeta = 0;
-
-      // PORCENTAJE PRECIO PISO
-      this.porpreciopisoGral = 0;
-      this.porcostoVta = 0;
-      this.porgastoCryo = 0;
-      this.porgastoDist = 0;
-      this.pordepreciacion = 0;
-      this.porutilidadOperativaSinGVyGADM = 0;
-      this.porgastoVta = 0;
-      this.porgastoAdm = 0;
-      this.porutilidadOperativaNeta = 0;
-
-      // COSTO PRECIO PROPUESTO
-      this.pppreciopisoGral = 0;
-      this.ppcostoVta = 0;
-      this.ppgastoCryo = 0;
-      this.ppgastoDist = 0;
-      this.ppdepreciacion = 0;
-      this.pputilidadOperativaSinGVyGADM = 0;
-      this.ppgastoVta = 0;
-      this.ppgastoAdm = 0;
-      this.pputilidadOperativaNeta = 0;
-
-      // PORCENTAJE PRECIO PROPUESTO
-      this.ppporpreciopisoGral = 0;
-      this.ppporcostoVta = 0;
-      this.ppporgastoCryo = 0;
-      this.ppporgastoDist = 0;
-      this.pppordepreciacion = 0;
-      this.ppporutilidadOperativaSinGVyGADM = 0;
-      this.ppporgastoVta = 0;
-      this.ppporgastoAdm = 0;
-      this.ppporutilidadOperativaNeta = 0;
-
-      // TOTALES PRECIO PISO
-      this.tpfacturacionAnual = 0;
-      this.tputilidadOperativaNeta = 0;
-      this.tputilidadOperativaNetaCryoInfra = 0;
-      this.tputilidadOperativaSinGVyGADM = 0;
-
-      // TOTALES PRECIO PROPUESTO
-      this.tppfacturacionAnual = 0;
-      this.tpputilidadOperativaNeta = 0;
-      this.tpputilidadOperativaNetaCryoInfra = 0;
-      this.tpputilidadOperativaSinGVyGADM = 0;
     })
 
   }
